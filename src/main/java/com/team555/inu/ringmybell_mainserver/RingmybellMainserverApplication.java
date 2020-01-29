a18691f6ce41a7045aa73a842d5ea392c21f37af
taehee-kim-dev <taehee.kim.dev@gmail.com>
2020-01-30 03:50:51 +0900

    Detach database configuration file

--- src/main/java/com/team555/inu/ringmybell_mainserver/RingmybellMainserverApplication.java
+++ src/main/java/com/team555/inu/ringmybell_mainserver/RingmybellMainserverApplication.java
@@ -2,10 +2,8 @@ package com.team555.inu.ringmybell_mainserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource(value = {"classpath:${jdbc.config}"})
 public class RingmybellMainserverApplication {
 
     public static void main(String[] args) {
