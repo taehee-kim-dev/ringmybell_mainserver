f8ee9d8dd7e4cb25b87cbe6272d00fac84a38b42
taehee-kim-dev <taehee.kim.dev@gmail.com>
2020-02-03 20:13:32 +0900

    Test파일 아무것도 안하도록 수정

--- src/test/java/com/team555/inu/ringmybell_mainserver/RingmybellMainserverApplicationTests.java
+++ src/test/java/com/team555/inu/ringmybell_mainserver/RingmybellMainserverApplicationTests.java
@@ -1,10 +1,7 @@
 package com.team555.inu.ringmybell_mainserver;
 
-
-import com.team555.inu.ringmybell_mainserver.server.dao.RingMyBellMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -12,16 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class RingmybellMainserverApplicationTests {
 
-    @Autowired
-    private RingMyBellMapper ringMyBellMapper;
-
     @Test
     public void test() {
-        try {
-            System.out.println(ringMyBellMapper.selectBusStopsListOf780_1());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 
 }
