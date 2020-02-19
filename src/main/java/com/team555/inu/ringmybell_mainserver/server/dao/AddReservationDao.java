f19b94201c0779c829525f62635b1e0ec78bdee4
taehee-kim-dev <taehee.kim.dev@gmail.com>
2020-02-19 10:44:23 +0900

    Add logs to AddReservationDao.java

--- src/main/java/com/team555/inu/ringmybell_mainserver/server/dao/AddReservationDao.java
+++ src/main/java/com/team555/inu/ringmybell_mainserver/server/dao/AddReservationDao.java
@@ -2,8 +2,10 @@ package com.team555.inu.ringmybell_mainserver.server.dao;
 
 import com.team555.inu.ringmybell_mainserver.server.vo.Android;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
+@Slf4j
 @RequiredArgsConstructor
 @Repository
 public class AddReservationDao {
@@ -15,6 +17,7 @@ public class AddReservationDao {
 
         try {
             result = ringMyBellMapper.countReservation(android);
+            log.info("현재 해당 안드로이드 식별자로 예약되어있는 데이터의 갯수 : " + result);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -22,12 +25,14 @@ public class AddReservationDao {
         if(result > 0){
             try {
                 result = ringMyBellMapper.updateReservation(android);
+                log.info("기존 예약되어있는 데이터가 존재하여 updateReservation 실행. result = " + result);
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }else if(result == 0){
             try {
                 result = ringMyBellMapper.addReservation(android);
+                log.info("기존 예약되어있는 데이터가 없어 addReservation 실행. result = " + result);
             } catch (Exception e) {
                 e.printStackTrace();
             }
